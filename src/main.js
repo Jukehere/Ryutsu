b9138ef994ccadac704a0d805009a0de2a442171
Panagiotis Sofianopoulos <sofianopoulospanagiotis@gmail.com>
2025-07-15 23:52:02 +0300

    Version 0.1
    Starting strong with some datacenters, price fetching works with HQ/NQ selection. Also displays a map at the end so you can visualise where to go next.

---
+++ src/main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  mainWindow.webContents.openDevTools();
+
+  // Optional debugger
+  mainWindow.webContents.on('did-finish-load', () => {
+    mainWindow.webContents.debugger.attach('1.1');
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
